6dac6623d6eecfff8dd54360c719f5ab8fffa2fd
pabloparejo <parejo.p@gmail.com>
2020-09-27 13:06:23 +0200

    Style name sticker component

--- src/components/nameSticker.js
+++ src/components/nameSticker.js
@@ -4,12 +4,23 @@ import PropTypes from 'prop-types';
 const NameSticker = props => {
   const { name } = props;
   return (
-    <div>
-      <h3>HELLO</h3>
-      <p>My name is</p>
-      <div>
-        <p>{name}</p>
-      </div>
+    <div
+      style={{
+        backgroundColor: 'red',
+        borderRadius: 12,
+        color: 'white',
+        textAlign: 'center',
+        paddingBottom: 1,
+        maxWidth: 300
+      }}
+    >
+      <h3 style={{ fontSize: 28, marginBottom: 0 }}>HELLO</h3>
+      <p style={{ margin: 0 }}>My name is</p>
+      <p
+        style={{ margin: '15px 0', color: 'black', fontSize: 30, padding: '20px 0', backgroundColor: 'white' }}
+      >
+        {name}
+      </p>
     </div>
   );
 };
